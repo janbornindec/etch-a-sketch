ecc56c95d08c188aef184234e32735af7f0e46d7
janbornindec <janellecwx@gmail.com>
2023-04-14 13:03:57 +1000

    Change square colour on hover

--- javascript.js
+++ javascript.js
@@ -1,17 +1,26 @@
 //select container div on html
 const container = document.querySelector('.container');
 
-
-for (i = 0; i <16; i++) {
+for (row = 0; row <16; row++) {
     //create 16 columns to hold squares
     const columns = document.createElement('div');
     columns.setAttribute('id','columns');
     container.appendChild(columns);
 
-    for (x = 0; x <16; x++) {
+    for (i = 0; i <16; i++) {
         //create square elements
-        const rows = document.createElement('div');
-        rows.setAttribute('id','rows');
-        columns.appendChild(rows);
+        const squares = document.createElement('div');
+        squares.setAttribute('id','squares');
+        columns.appendChild(squares);
     };
 };
+
+//select the squares
+const squares = document.querySelectorAll('#squares');
+
+//change square colour when mouse hovers it
+squares.forEach((square) => {
+    square.addEventListener('mouseover',() => {
+        square.style.backgroundColor = 'black';
+    });
+});

e0079105774db40319474bd85149ea1a5db9c766
janbornindec <janellecwx@gmail.com>
2023-04-14 14:23:54 +1000

    Add getsize prompt and create new pad func

--- javascript.js
+++ javascript.js
@@ -1,7 +1,8 @@
 //select container div on html
 const container = document.querySelector('.container');
 
-for (row = 0; row <16; row++) {
+//create default pad
+for (x = 0; x <16; x++) {
     //create 16 columns to hold squares
     const columns = document.createElement('div');
     columns.setAttribute('id','columns');
@@ -15,8 +16,45 @@ for (row = 0; row <16; row++) {
     };
 };
 
+const button = document.querySelector('button');
+let columnNum;
+let rowNum;
+
+function getSize() {
+    columnNum = prompt("Enter sketch pad's column number: ");
+    rowNum = prompt("Enter sketch pad's row number: ");
+};
+
+//trigger event once button is clicked
+button.addEventListener('click',newPad);
+
+//create a new pad according to user's instructed size
+function newPad() {
+
+    //clear previous pad
+    container.textContent = '';
+
+    //get user to enter pad's size
+    getSize();
+
+    //implement the number user typed
+    for (x = 0; x < (columnNum-1); x++) {
+        //create 16 columns to hold squares
+        const columns = document.createElement('div');
+        columns.setAttribute('id','columns');
+        container.appendChild(columns);
+
+        for (i = 0; i < (rowNum-1); i++) {
+            //create square elements
+            const rows = document.createElement('div');
+            rows.setAttribute('id','squares');
+            columns.appendChild(rows);
+        };
+    };
+}
+
 //select the squares
-const squares = document.querySelectorAll('#squares');
+const squares = document.querySelectorAll('#rows');
 
 //change square colour when mouse hovers it
 squares.forEach((square) => {
